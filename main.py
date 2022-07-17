0dc8585e42c4db5873de5c8756180ee69e5064f5
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2022-07-18 00:15:28 +0530

    test with spotify

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
-from quart import Quart
+from tokenize import String
+from turtle import up
+from matplotlib.font_manager import json_dump
+from quart import Quart, request
 from telegram.ext.updater import Updater
 from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
 from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
+import telegram
 import os
 
 telegram_bot_token="5409399469:AAGRoB-pli3h6hcF42kvjyPbNBirjeUaUdg"
+chat_ids = set()
   
+bot = telegram.Bot(telegram_bot_token)
 updater = Updater("5409399469:AAGRoB-pli3h6hcF42kvjyPbNBirjeUaUdg",
                   use_context=True)
   
   
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text("""
-    HelloAutoMateBot.                          
-    """)
+    if(update.message.chat.id in chat_ids):
+        update.message.reply_text(f'''
+        Hey {update.message.chat.first_name}. You've already subscribed to see Abhishek's playlist                         
+        ''')
+    else:
+        chat_ids.add(update.message.chat.id)
+        update.message.reply_text(f'''
+        Hey {update.message.chat.first_name}. You've just subscribed to see Abhishek's playlist                         
+        ''')
   
-def help(update: Update, context: CallbackContext):
-    update.message.reply_text("""""")
-    
-def unknown_text(update: Update, context: CallbackContext):
-    update.message.reply_text("""unknown command bro""")
-    
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('help', help))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, start))
 
 updater.start_polling()
   
@@ -41,3 +46,12 @@ app = Quart(__name__)
 @app.route('/')
 async def hello():
     return 'hello'
+
+@app.route('/triggerSpotify', methods=['GET','POST'])
+async def triggerSpotify():
+    if request.method =='POST':
+        message = str(await request.get_json())
+        for id in chat_ids:
+            bot.send_message(text=f'Abhishek just liked {message}', chat_id=id)
+        return "done"    
+        
