9b9f43f8b42e4dd162876792758b820a108c28aa
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2022-07-18 03:05:11 +0530

    prod

--- main.py
+++ main.py
@@ -8,47 +8,61 @@ from telegram.ext.filters import Filters
 import telegram
 import os
 
-telegram_bot_token="5409399469:AAGRoB-pli3h6hcF42kvjyPbNBirjeUaUdg"
-chat_ids = set()
-  
-bot = telegram.Bot(telegram_bot_token)
-updater = Updater("5409399469:AAGRoB-pli3h6hcF42kvjyPbNBirjeUaUdg",
-                  use_context=True)
-  
-  
-def start(update: Update, context: CallbackContext):
-    if(update.message.chat.id in chat_ids):
-        update.message.reply_text(f'''
-        Hey {update.message.chat.first_name}. You've already subscribed to see Abhishek's playlist                         
-        ''')
-    else:
-        chat_ids.add(update.message.chat.id)
-        update.message.reply_text(f'''
-        Hey {update.message.chat.first_name}. You've just subscribed to see Abhishek's playlist                         
-        ''')
-  
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, start))
-
-updater.start_polling()
-  
-# updater.start_webhook(listen="0.0.0.0",
-#                       port=int(os.environ.get('PORT', 5000)),
-#                       url_path=telegram_bot_token,
-#                       webhook_url= "https://helloautomate.herokuapp.com/" + telegram_bot_token
-#                       )
-
 app = Quart(__name__)
 
-@app.route('/')
-async def hello():
-    return 'hello'
+telegram_bot_token="5409399469:AAGRoB-pli3h6hcF42kvjyPbNBirjeUaUdg"
+subscribed_ids = set()
+  
+bot = telegram.Bot(telegram_bot_token)
+hook = bot.set_webhook("https://helloautomate.herokuapp.com/")
+if not hook:
+    print("Webhook setup failed. exiting.");
+    exit(0)
+
+
+@app.route('/', methods=['GET','POST'])
+async def getMessages():
+    try:
+        if request.method=='GET':
+            return "Hello"
+        elif request.method=='POST':
+            req = await request.get_json(force=True)
+            update = Update.de_json(req, bot)
+            text = update.message.text.lower()
+            chat_id = update.message.chat.id
+            name = update.message.chat.first_name
+            if text in ['start','/start']:
+                bot.send_message(chat_id = chat_id, text= f''' Hello 👋, {name}. commands: /spotify (Subscribe to get notified when Abhishek likes a song on Spotify) ''')
+                return "201"
+            elif text in ['spotify', '/spotify']:
+                if chat_id in subscribed_ids:
+                    bot.send_message(chat_id = chat_id, text=''' You've already subscribed to Spotify updates. send /ewspotify or /stopspotify to unsubscribe.''' )
+                    return "201"
+                else:
+                    subscribed_ids.add(chat_id)
+                    bot.send_message(chat_id = chat_id, text=''' You just subscribed to Spotify updates. send /ewSpotify or /stopSpotify to unsubscribe.''')
+                    return "201"
+            elif text in ['/ewspotify', '/stopspotify', 'stop spotify', 'ew spotify']:
+                subscribed_ids.remove(chat_id)
+                bot.send_message(chat_id = chat_id, text=''' You just unsubscribed to Spotify updates. Thankyou ''')
+                return "201"
+            else:
+                bot.send_message(chat_id = chat_id, text="unknown command bro")
+                return "201"
+    except:
+        return "Something went wrong." 
 
 @app.route('/triggerSpotify', methods=['GET','POST'])
 async def triggerSpotify():
-    if request.method =='POST':
-        message = str(await request.get_json())
-        for id in chat_ids:
-            bot.send_message(text=f'Abhishek just liked {message}', chat_id=id)
-        return "done"    
+    try:
+        if request.method =='POST':
+            data = await request.get_json()
+            info = list(data.values())
+            for id in subscribed_ids:
+                bot.send_message(text=f'Abhishek just liked {info[2]} by {info[0]}. {info[1]}', chat_id=id)
+        return "done" 
+    except:
+        print('An exception occurred')
+        return "done"
+       
         
