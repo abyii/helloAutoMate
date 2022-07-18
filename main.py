e177158856ac8784262e441434eb6b5da892ceeb
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2022-07-18 23:56:02 +0530

    mongo

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ from telegram.update import Update
 import telegram
 import os
 from dotenv import load_dotenv
+from database import get_database
 
 load_dotenv()
 
 app = Quart(__name__)
 
-telegram_bot_token=os.environ['TELE_KEY']
+db = get_database()
+people = db["people"]
+
 subscribed_ids = set()
-  
-bot = telegram.Bot(telegram_bot_token)
-hook = bot.set_webhook("https://helloautomate.herokuapp.com/")
+bot = telegram.Bot(os.environ['TELE_KEY'])
+hook = bot.set_webhook("https://d4a1-2409-4071-2283-f481-9dc1-f1f5-dd02-c7b1.in.ngrok.io/")
 if not hook:
     print("Webhook setup failed. exiting.");
     exit(0)
-
 
 @app.route('/', methods=['GET','POST'])
 async def getMessages():
@@ -29,20 +30,44 @@ async def getMessages():
             text = update.message.text.lower()
             chat_id = update.message.chat.id
             name = update.message.chat.first_name
+            person_id = update.message.from_user.id
+            username = update.message.from_user.username
             if text in ['start','/start']:
-                bot.send_message(chat_id = chat_id, text= f''' Hello 👋, {name}. commands: /spotify (Subscribe to get notified when Abhishek likes a song on Spotify) ''')
+                bot.send_message(chat_id = chat_id, 
+text= 
+f''' 
+Hello 👋, {name}. 
+commands: 
+/spotify (Subscribe to get notified when there's a new song on da playlist..) 
+/stopSpotify (Unsubscribe from getting spotify updates)
+    ''')
                 return "201"
             elif text in ['spotify', '/spotify']:
-                if chat_id in subscribed_ids:
+                person = people.find_one({"chat_id":chat_id})
+                if person:
                     bot.send_message(chat_id = chat_id, text=''' You've already subscribed to Spotify updates. send /ewspotify or /stopspotify to unsubscribe.''' )
                     return "201"
                 else:
-                    subscribed_ids.add(chat_id)
+                    person = people.find_one({"id":person_id})
+                    if person:
+                        people.update_one({"id":person_id}, {"$set":{"chat_id":chat_id}})
+                    else:
+                        people.insert_one({
+                            "name":name,
+                            "id":person_id,
+                            "chat_id":chat_id,
+                            "username":username,
+                            "preferSpotify":True
+                        })
                     bot.send_message(chat_id = chat_id, text=''' You just subscribed to Spotify updates. send /ewSpotify or /stopSpotify to unsubscribe.''')
                     return "201"
             elif text in ['/ewspotify', '/stopspotify', 'stop spotify', 'ew spotify']:
-                subscribed_ids.remove(chat_id)
-                bot.send_message(chat_id = chat_id, text=''' You just unsubscribed to Spotify updates. Thankyou ''')
+                person = people.find_one({"chat_id":chat_id})
+                if person and person['preferSpotify']:
+                    people.update_one({"chat_id":chat_id}, {"$set":{"preferSpotify":False}})
+                    bot.send_message(chat_id = chat_id, text=''' You just unsubscribed to Spotify updates. Thankyou ''')
+                else:
+                    bot.send_message(chat_id = chat_id, text='''You haven't subscribed to unsubscibe.''')
                 return "201"
             else:
                 bot.send_message(chat_id = chat_id, text="unknown command bro")
@@ -56,8 +81,9 @@ async def triggerSpotify():
         if request.method =='POST':
             data = await request.get_json()
             info = list(data.values())
-            for id in subscribed_ids:
-                bot.send_message(text=f'New song on da playlist! {info[2]} by {info[0]}. {info[1]}', chat_id=id)
+            friends = people.find({"preferSpotify":True})
+            for friend in friends:
+                bot.send_message(text=f'New song on da playlist! {info[2]} by {info[0]}. {info[1]}', chat_id=friend["chat_id"])
         return "done" 
     except:
         print('An exception occurred')
